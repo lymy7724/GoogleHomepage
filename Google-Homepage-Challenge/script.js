e401319bf9c1eece4adaea4b36c775369cc8d8b2
Ly My Y <lymy7724@gmail.com>
2024-04-06 14:11:59 -0400

    updated script

--- Google-Homepage-Challenge/script.js
+++ Google-Homepage-Challenge/script.js
@@ -1,8 +1,10 @@
 function showAlert() {
-  alert ("You clicked I'm Feeling Lucky!");
+  alert("You clicked I'm Feeling Lucky!");
 }
 
 
+
+//get form element
 let searchForm = document.getElementById("search");
 
 // Take over form submission
@@ -10,16 +12,16 @@ searchForm.addEventListener("submit", (event) => {
   event.preventDefault();        // Stops form from doing what it usually does
   const inputs = document.getElementById("q");
   console.log(inputs.value)
-  if(inputs.value === "advanced search"){
+  if (inputs.value === "advanced search") {
     window.location.href = "advsearch.html";
   }
-  else if (inputs.value === "google images"){
+  else if (inputs.value === "google images") {
     window.location.href = "images.html";
   }
-  else if (inputs.value === ""){
+  else if (inputs.value === "") {
     alert("Please enter something to search for")
   }
-  else{
+  else {
     window.location.href = "https://www.google.com/search?q=" + inputs.value;
   }
 });
