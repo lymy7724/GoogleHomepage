9295cf7c1d86f1960d4463e3beb97e5a88752c81
Ly My Y <lymy7724@gmail.com>
2024-04-06 14:09:32 -0400

    uploaded files

---
+++ Google-Homepage-Challenge/script.js
@@ -0,0 +1,25 @@
+function showAlert() {
+  alert ("You clicked I'm Feeling Lucky!");
+}
+
+
+let searchForm = document.getElementById("search");
+
+// Take over form submission
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();        // Stops form from doing what it usually does
+  const inputs = document.getElementById("q");
+  console.log(inputs.value)
+  if(inputs.value === "advanced search"){
+    window.location.href = "advsearch.html";
+  }
+  else if (inputs.value === "google images"){
+    window.location.href = "images.html";
+  }
+  else if (inputs.value === ""){
+    alert("Please enter something to search for")
+  }
+  else{
+    window.location.href = "https://www.google.com/search?q=" + inputs.value;
+  }
+});
